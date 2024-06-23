79c1a0324f78cffc2204d4b226024f4f9e380286
Kaoticz <1812311+Kaoticz@users.noreply.github.com>
2024-06-23 01:00:45 -0300

    Added lerping to mob movement

--- Scripts/Mob.gd
+++ Scripts/Mob.gd
@@ -14,6 +14,12 @@ var movements: int = 0
 ## Defines whether the mobs are moving upwards or downwards.
 var going_down: bool = true
 
+## Indicates whether this mob has started moving since spawning.
+var started_moving: bool = false
+
+## The position the mob should lerp to.
+var target_position: Vector2 = self.position
+
 
 ## Start method, called when this node enters the scene tree for the first time.
 func _ready() -> void:
@@ -23,23 +29,26 @@ func _ready() -> void:
 
 ## Update method, runs on every frame.
 ## delta: The elapsed time since the previous frame.
-func _process(_delta: float) -> void:
-	pass
+func _process(delta: float) -> void:
+	if started_moving:
+		self.position = self.position.lerp(target_position, delta * 3)
 
 
 ## Moves the mobs everytime the $MoveTimer ticks.
 func _on_timer_timeout() -> void:
 	if going_down && movements < 8:
-		self.position.y += 30
+		target_position = Vector2(self.position.x, self.position.y + 30)
 		movements += 1
 	elif !going_down && movements < 8:
-		self.position.y -= 30
+		target_position = Vector2(self.position.x, self.position.y - 30)
 		movements += 1
 	else:
-		self.position.x -= 60
+		target_position = Vector2(self.position.x - 60, self.position.y)
 		going_down = !going_down
 		movements = 0
 		$MoveTimer.wait_time *= 0.9
+	
+	started_moving = true
 	
 	if randi() % 101 < 10:
 		var projectile: Area2D = laser_scene.instantiate()
